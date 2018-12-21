00cf1773939e8663a4aae1d39ee25b37e76cc2b1
Thế Anh <theanhnb97@users.noreply.github.com>
2018-12-21 10:40:25 +0700

    Initial commit

---
+++ Desktop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Shell32;
+
+namespace Desktop
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Shell shellObject = new Shell();
+            shellObject.ToggleDesktop();
+        }
+    }
+}
